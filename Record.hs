bca9a635e186f4f2f415eaec24921031e3948178
Marco <m.gapke@ostfalia.de>
2025-11-13 08:49:06 +0100

    Verschoben in  insert.hs

--- Record.hs
+++
@@ -1,6 +0,0 @@
--- Definition eines Records
-data Record = Record
-  { id    :: Int      -- Feld 'id' vom Typ Int
-  , name  :: String   -- Feld 'name' vom Typ String
-  , value :: Double   -- Feld 'value' vom Typ Double
-  } deriving (Show, Eq)
