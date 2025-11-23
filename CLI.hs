6016d5f270e93d47e357f350f5576441aa5c9a74
G.So <g.sommer@ostfalia.de>
2025-11-23 22:17:16 +0100

    Implement filter and query commands in CLI with small error handling; updated README.MD

--- CLI.hs
+++ CLI.hs
@@ -4,12 +4,15 @@ module CLI (handleCommand) where
 import Record
 import DataHandler
 
+import Data.Char (toLower) -- Query: non case-sensitive search
+import Text.Read (readMaybe) -- Fehlerbehandlung beim lesen
+
 -- Entscheidet, welcher Befehl ausgeführt wird
 handleCommand :: String -> [String] -> IO ()
 handleCommand "--insert" args = handleInsert args
 handleCommand "--delete" args = handleDelete args         
-                                                        
-                                                        -- @Gry  TODO  filter und fuery müssen hier augerufen werden 
+handleCommand "--filter" args = handleFilter args
+handleCommand "--query"  args = handleQuery args
                                                             
 handleCommand cmd _           = putStrLn ("Unbekannter Befehl: " ++ cmd)
 
@@ -63,10 +66,48 @@ handleDelete _ =
 -- filter (Task 7)
 --------------------------------------------------
 
--- @Gry
+handleFilter :: [String] -> IO ()
+handleFilter (file:thresholdStr:_) = 
+    case readMaybe thresholdStr of
+        Just threshold -> do
+            records <- loadRecords file
+            let filtered = filter (\r -> value r > threshold) records
+            putStrLn ("Eintreage mit Wert groeßer als " ++ show threshold ++ ":")
+            mapM_ print filtered
+        Nothing -> putStrLn "Fehler: Wert muss eine Zahl sein."
+
+handleFilter _ = putStrLn "Benutzung: --filter <Datei> <Wert>"
 
 --------------------------------------------------
 -- query (Task 8)
 --------------------------------------------------
 
--- @Gry
+handleQuery :: [String] -> IO ()
+handleQuery (file:searchStr:_) = do
+    -- Einträge laden
+    records <- loadRecords file
+
+    -- Gefundene Einträge anzeigen (non case-sensitive)
+    let searchLower = map toLower searchStr
+    let found = filter (\r -> contains searchLower (map toLower (Record.name r))) records
+
+    putStrLn ("Eintraege mit Name, der \"" ++ searchStr ++ "\" enthaelt:")
+    mapM_ print found
+
+handleQuery _ = putStrLn "Benutzung: --query <Datei> <Suchbegriff>"
+
+-- Hilfsfunktion: Prüft, ob sub in s enthalten ist
+contains :: String -> String -> Bool
+contains [] _ = True
+contains _ [] = False
+contains sub (c:cs)
+    | startsWith sub (c:cs) = True
+    | otherwise = contains sub cs
+
+-- Hilfsfunktion: Prüft, ob s mit prefix beginnt
+startsWith :: String -> String -> Bool
+startsWith [] _ = True
+startsWith _ [] = False
+startsWith (p:ps) (s:ss)
+    | p == s    = startsWith ps ss
+    | otherwise = False
